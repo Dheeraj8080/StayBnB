4ececbe123639c0f860f1bb858eb23d5fa8c3701
unknown <dheerajsharma121002@gmail.com>
2023-11-25 16:46:20 +0530

    vercel4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import PlacePage from "./pages/PlacePage";
 import AccountPage from "./pages/AccountPage";
 import BookingsPage from "./pages/BookingsPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
